275a3e3e089d2175543ca074199b9147df124c62
William Wennerström <william@willeponken.me>
2019-02-01 16:24:22 +0100

    stream: rewrite with threading

--- experiment/webcam_stream/stream.py
+++ experiment/webcam_stream/stream.py
@@ -1,113 +1,100 @@
+import atexit
+import multiprocessing as mp
+import signal
+import sys
+
 import cv2
 import face_recognition
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+def encode_face(path):
+    print("Encoding image: %s" % path)
+    image = face_recognition.load_image_file(path)
+    face_encoding = face_recognition.face_encodings(image)[0]
+    print("Successfully encoded: %s" % path)
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+    return face_encoding
 
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+def compare_face(known_face_names, known_face_encodings, face_encoding):
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    name = "Unknown"
 
-william_image = face_recognition.load_image_file("william.jpg")
-william_face_encoding = face_recognition.face_encodings(william_image)[0]
+    # If a match was found in known_face_encodings, just use the first one.
+    if True in matches:
+        first_match_index = matches.index(True)
+        name = known_face_names[first_match_index]
 
-philip_image = face_recognition.load_image_file("philip.jpg")
-philip_face_encoding = face_recognition.face_encodings(philip_image)[0]
+    return name
 
-edvin_image = face_recognition.load_image_file("edvin.jpg")
-edvin_face_encoding = face_recognition.face_encodings(edvin_image)[0]
 
-rebecka_image = face_recognition.load_image_file("rebecka.jpg")
-rebecka_face_encoding = face_recognition.face_encodings(rebecka_image)[0]
+def find_faces(people, camera_index):
+    # The thread pool is used to run multiple encoders and recognition processes in parallel.
+    thread_pool = mp.Pool(None, lambda: signal.signal(signal.SIGINT, signal.SIG_IGN))
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding,
-    william_face_encoding,
-    philip_face_encoding,
-    edvin_face_encoding,
-    rebecka_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden",
-    "William Wennerstrom",
-    "Philip Hjortsberg",
-    "Edvin Sladic",
-    "Rebecka Skarin"
-]
+    try:
+        # Spawn one thread for each face encoder.
+        encode_thread = thread_pool.map_async(encode_face, [(p['path']) for p in people])
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+        # While encoding the faces, open the video capture (webcam).
+        video_capture = cv2.VideoCapture(camera_index)
+        atexit.register(video_capture.release)  # Clean exit by releasing webcam on interrupt.
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+        known_face_names = [p["name"] for p in people]
+        known_face_encodings = encode_thread.get(timeout=120)  # Collect result from threads, w/ timeout 2m.
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_frame = frame[:, :, ::-1]
+        face_locations = []
+        face_encodings = []
+        while True:
+            # Grab a single frame of video
+            ret, frame = video_capture.read()
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_frame)
-        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses).
+            rgb_frame = frame[:, :, ::-1]
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+            # Find all the faces and face encodings in the current frame of video.
+            face_locations = face_recognition.face_locations(rgb_frame)
+            face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-            # If a match was found in known_face_encodings, just use the first one.
-            if True in matches:
-                first_match_index = matches.index(True)
-                name = known_face_names[first_match_index]
+            # Compare faces and display the results.
+            for name in thread_pool.starmap(
+                    compare_face,
+                    [(known_face_names, known_face_encodings, e) for e in face_encodings]):
+                print(name)
 
-            face_names.append(name)
+    except KeyboardInterrupt:
+        thread_pool.terminate()
+        thread_pool.join()
 
-    process_this_frame = not process_this_frame
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 1
-        right *= 1
-        bottom *= 1
-        left *= 1
-
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+find_faces([
+    {
+        "name": "William",
+        "path": "william.jpg"
+    },
+    {
+        "name": "Philip",
+        "path": "philip.jpg"
+    },
+    {
+        "name": "Rebecka",
+        "path": "rebecka.jpg"
+    },
+    {
+        "name": "Obama",
+        "path": "obama.jpg"
+    },
+    {
+        "name": "Edvin",
+        "path": "edvin.jpg"
+    },
+    {
+        "name": "Biden",
+        "path": "biden.jpg"
+    },
+    {
+        "name": "Hampus",
+        "path": "hampus.jpg"
+    }
+], int(sys.argv[1]))
