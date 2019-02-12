d69f19913aeea3e9d6dd2bcdf3680edae1f1ada4
edvin <sladic.edvin@gmail.com>
2019-02-12 13:10:36 +0100

    Started on implementing OAuth

--- fitbit-server/fitbit-server.go
+++ fitbit-server/fitbit-server.go
@@ -6,17 +6,34 @@ import (
 	"log"
 )
 
+type authParams struct {
+	client_id string
+	response_type string
+	scope string
+	redirect_uri string
+}
+
 func answer(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r)
 	fmt.Fprintf(w, "AWAKE")
 }
 
+func register(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, registerUrl, http.StatusSeeOther)
+}
+
+func success(w http.ResponseWriter, r *http.Request)  {
+
+}
+
 func fitbitData(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNoContent)		
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
 	http.HandleFunc("/", answer)
+	http.HandleFunc("/register/", register)
+	http.HandleFunc("/register/success", success)
 	http.HandleFunc("/subscribe/sleep/", fitbitData)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
