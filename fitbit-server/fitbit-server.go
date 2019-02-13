8c51c719457c73d8e2a55c124b12b5a3f9c9ec60
edvin <sladic.edvin@gmail.com>
2019-02-13 15:53:04 +0100

    Fixed authorization against fitbit servers. Left to do is handle callback.

--- fitbit-server/fitbit-server.go
+++ fitbit-server/fitbit-server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
+	"os"
 )
+
+var params authParams
 
 type authParams struct {
 	client_id string
@@ -19,11 +22,14 @@ func answer(w http.ResponseWriter, r *http.Request) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
+	var registerUrl = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=" + params.client_id +
+		"&redirect_uri=phewstoc.sladic.se/success/&scope=sleep"
 	http.Redirect(w, r, registerUrl, http.StatusSeeOther)
 }
 
 func success(w http.ResponseWriter, r *http.Request)  {
-
+	fmt.Print(r)
+	fmt.Fprintf(w, "Received! :)")
 }
 
 func fitbitData(w http.ResponseWriter, r *http.Request) {
@@ -31,10 +37,13 @@ func fitbitData(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	params = authParams{os.Args[1], os.Args[2], os.Args[3], os.Args[4]}
+
 	http.HandleFunc("/", answer)
 	http.HandleFunc("/register/", register)
 	http.HandleFunc("/register/success", success)
 	http.HandleFunc("/subscribe/sleep/", fitbitData)
+
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("listenAndServe: ", err)
