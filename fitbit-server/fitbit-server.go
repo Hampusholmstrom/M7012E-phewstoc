97fbd872a47a7eac3181a7dc668811741d29d069
edvin <sladic.edvin@gmail.com>
2019-03-05 10:58:45 +0100

    added resting heart rate and refactored the big OnSucessAuth-function

--- fitbit-server/fitbit-server.go
+++ fitbit-server/fitbit-server.go
@@ -12,13 +12,22 @@ import (
 )
 
 var params authParams
-
 type authParams struct {
 	client_id     string
 	client_secret string
 	response_type string
 	scope         string
 	redirect_uri  string
+	refresh_token string
+}
+
+var accessTokenInfo ResponseInfo
+type ResponseInfo struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	TokenType    string `json:"token_type"`
+	UserId       string `json:"user_id"`
 }
 
 func welcomeMessage(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +50,15 @@ type Calorie float64
 
 type Heart struct {
 	ActivitiesHeartIntraday ActivityHeartIntraday `json:"activities-heart-intraday"`
+	ActivitiesHeart 				ActivitiesHeart				`json:"activities-heart"`
+}
+
+type ActivitiesHeart struct {
+	Dataset  []RestingHeartRate `json:"dataset"`
+}
+
+type RestingHeartRate struct {
+	HeartRate HeartRate `json:"restingHeartRate"`
 }
 
 type ActivityHeartIntraday struct {
@@ -85,21 +103,22 @@ func authOnSuccess(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(resp.Status)
-	type ResponseInfo struct {
-		AccessToken  string `json:"access_token"`
-		ExpiresIn    int    `json:"expires_in"`
-		RefreshToken string `json:"refresh_token"`
-		TokenType    string `json:"token_type"`
-		UserId       string `json:"user_id"`
-	}
-	var accessTokenInfo ResponseInfo
+
 	err = json.NewDecoder(resp.Body).Decode(&accessTokenInfo)
 	if err != nil {
 		fmt.Println("error:", err)
+	} else {
+		authParams.refresh_token = accessTokenInfo.RefreshToken
 	}
-	fmt.Println(accessTokenInfo)
+	//fmt.Println(accessTokenInfo)
+	heartRate := getHeartRateData()
+	fmt.Fprint(w, heartRate.Heart)
+	//fmt.Fprint(w, heartRate.ActivitiesHeartIntraday)
+	//fmt.Fprint(w, heartRate.ActivitiesHeart)
+}
 
-    reqURL := "https://api.fitbit.com/1/user/" + accessTokenInfo.UserId + "/activities/heart/date/today/1d/1sec/time/05:00/05:01.json"
+func getHeartRateData() Heart {
+	reqURL := "https://api.fitbit.com/1/user/" + accessTokenInfo.UserId + "/activities/heart/date/today/1d/1sec/time/05:00/05:01.json"
 	getReq, err := http.NewRequest("GET", reqURL, nil)
 	if err != nil {
 		fmt.Println("some get reqq err")
@@ -113,17 +132,48 @@ func authOnSuccess(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(getResp.Status)
 
 	var heartRate Heart
-	//	body, err := ioutil.ReadAll(getResp.Body)
-	//	fmt.Println(string(body))
 	err = json.NewDecoder(getResp.Body).Decode(&heartRate)
-	fmt.Fprint(w, heartRate.ActivitiesHeartIntraday)
-	// fmt.Println(sleepLog.Sleep[0].Data)
-	// fmt.Fprint(w, getResp)
-	// fmt.Println(resp)
+	return heartRate
 }
 
 func fitbitData(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func refreshToken(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+
+	v := url.Values{}
+	v.Add("grant_type", "authorization_code")
+	v.Add("refresh_token", authParams.refresh_token)
+
+	req, err := http.NewRequest("POST", "https://api.fitbit.com/oauth2/token", strings.NewReader(v.Encode()))
+	if err != nill {
+		fmt.Println("Unable to create request.")
+	}
+
+	req.Header.Set("Authorization", "Basic "+concAuth(params.client_id, params.client_secret))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("some client Do err")
+	}
+}
+
+func isSleeping(w http.ResponseWriter, r *http.Request) {
+	// Call on register to retrive data from FitBit
+
+	lowerbpm := findLowerHeartBeat()
+	upperbpm := findUpperHeartBeat()
+	timespan := findTimespanHeartBeat()
+
+	if upperbpm - upperbpm <= 10 && timespan <= 10 {
+		// return true
+	} else {
+		// return false
+	}
 }
 
 func main() {
@@ -133,6 +183,8 @@ func main() {
 	http.HandleFunc("/register/", register)
 	http.HandleFunc("/success/", authOnSuccess)
 	http.HandleFunc("/subscribe/sleep/", fitbitData)
+	http.HandleFunc("/refresh_token/", refreshToken)
+	http.HandleFunc("/is_sleeping/", isSleeping)
 
 	err := http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/phewstoc.sladic.se/fullchain.pem", "/etc/letsencrypt/live/phewstoc.sladic.se/privkey.pem", nil)
 	if err != nil {
